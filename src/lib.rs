c4d2e49de8f121684978a823e2b060075b1bfb43
Donald <dlqs@gmx.com>
2020-01-13 23:18:24 +0800

    add test for one tick

--- src/lib.rs
+++ src/lib.rs
@@ -65,7 +65,7 @@ impl Universe {
         let width = 64;
         let height = 64;
         let cells = (0..width * height)
-            .map(|i| {
+            .map(|_i| {
                 if Math::random() > 0.5 {
                     Cell::Alive
                 } else {
@@ -95,8 +95,28 @@ impl Universe {
     pub fn cells(&self) -> *const Cell {
         self.cells.as_ptr()
     }
+
+    pub fn set_width(&mut self, width: u32) {
+        self.width = width;
+        self.cells = (0..self.width * self.height).map(|_i| Cell::Dead).collect();
+    }
+    pub fn set_height(&mut self, height: u32) {
+        self.height = height;
+        self.cells = (0..self.width * self.height).map(|_i| Cell::Dead).collect();
+    }
 }
 
+impl Universe {
+    pub fn get_cells(&self) -> &[Cell] {
+        &self.cells
+    }
+    pub fn set_cells(&mut self, cells: &[(u32, u32)]) {
+        for (row, col) in cells.iter().cloned() {
+            let idx = self.get_index(row,col);
+            self.cells[idx] = Cell::Alive;
+        }
+    }
+}
 
 impl fmt::Display for Universe {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
